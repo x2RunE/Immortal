d9021ffc39a8f5c6d1c157e41e05e18e4761dcc7
x2rune <zeenogenesis.space@proton.me>
2026-07-13 16:17:25 +0700

    Create BananaCat-V4.lua

---
+++ BananaCat-V4.lua
@@ -0,0 +1,2 @@
+local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v1/loaders/541uf492jcm5vlq5w3p172iax2wssjpe.lua'))()
