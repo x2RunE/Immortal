8f0d54c582d8512370020ad8929cd827b339c984
x2rune <zeenogenesis.space@proton.me>
2026-07-15 03:11:16 +0700

    Update BananaCat-Loader.lua

--- BananaCat-Loader.lua
+++ BananaCat-Loader.lua
@@ -1,2 +1,2 @@
 local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
-loadstring(HttpGet('https://api.imt-hub.xyz/files/v1/loaders/2fjgshkp97ydh9x546qfax2ytxywuvqx.lua'))()
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/2fjgshkp97ydh9x546qfax2ytxywuvqx.lua'))()
