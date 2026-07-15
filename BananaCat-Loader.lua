667593780f76572871ff6644a05830dd9637f404
x2rune <zeenogenesis.space@proton.me>
2026-07-15 21:44:56 +0700

    Update BananaCat-Loader.lua

--- BananaCat-Loader.lua
+++ BananaCat-Loader.lua
@@ -1,2 +1,5 @@
+getgenv().Radiant = {
+    ACTIVE_KEY = '2c10751f4e4a2eef79e8b35089723423'
+}
 local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
 loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/2fjgshkp97ydh9x546qfax2ytxywuvqx.lua'))()
