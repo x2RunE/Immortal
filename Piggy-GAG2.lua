35d62d42bf9f559ccf231e12468f78ab4da05d9c
x2rune <zeenogenesis.space@proton.me>
2026-07-15 17:32:12 +0700

    Create Piggy-GAG2.lua

---
+++ Piggy-GAG2.lua
@@ -0,0 +1,5 @@
+if getgenv().Radiant then
+  getgenv().Radiant.ACTIVE_KEY = "f952a93a3d0712034a792521e6390f02"
+end
+local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/o4g8hi7wuc5n0kafgijaq9qgv81hatoz.lua'))()
