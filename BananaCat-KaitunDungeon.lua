34c19c8c86cd9306302772d1535f3fccaf5dce24
x2rune <zeenogenesis.space@proton.me>
2026-07-22 02:57:12 +0700

    Create BananaCat-KaitunDungeon.lua

---
+++ BananaCat-KaitunDungeon.lua
@@ -0,0 +1,5 @@
+getgenv().Radiant = {
+    ACTIVE_KEY = '2c10751f4e4a2eef79e8b35089723423'
+}
+local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/98daj1ykiv4t96ahl8ehw0qxmmqn4kob.lua'))()
