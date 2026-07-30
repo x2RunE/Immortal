234c984521241578441c6cb3a35ff7edd647d140
x2rune <zeenogenesis.space@proton.me>
2026-07-31 02:02:27 +0700

    Create BloxKidKaitun-Loader.lua

---
+++ BloxKidKaitun-Loader.lua
@@ -0,0 +1,2 @@
+local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/fi8rshwlwf9diobzyowpgwkkigpx4zm0.lua'))()
