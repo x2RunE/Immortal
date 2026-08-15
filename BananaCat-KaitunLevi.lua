5f01688eecccaf749eed742120861773837e5623
x2rune <zeenogenesis.space@proton.me>
2026-08-15 16:48:51 +0700

    Create BananaCat-KaitunLevi.lua

---
+++ BananaCat-KaitunLevi.lua
@@ -0,0 +1,2 @@
+local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/x81imt49wu2z3i0cnje3tp3g4mtm5wyj.lua'))()
