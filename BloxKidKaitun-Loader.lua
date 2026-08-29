527f0345ab7edd5b52d9715030b0a3c363c6476c
x2rune <zeenogenesis.space@proton.me>
2026-08-30 03:32:10 +0700

    Update BloxKidKaitun-Loader.lua

--- BloxKidKaitun-Loader.lua
+++ BloxKidKaitun-Loader.lua
@@ -1,2 +1,2 @@
 local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
-loadstring(HttpGet('http://187.52.122.114:3000/files/v2/loaders/fi8rshwlwf9diobzyowpgwkkigpx4zm0.lua'))()
+loadstring(HttpGet('https://api.imt-hub.xyz/files/v2/loaders/fi8rshwlwf9diobzyowpgwkkigpx4zm0.lua'))()
