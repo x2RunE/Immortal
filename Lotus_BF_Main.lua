64f25eaa26ea3fc81f71ef7dd5c41091f021263f
x2rune <zeenogenesis.space@proton.me>
2026-09-05 01:37:32 +0700

    Update Lotus_BF_Main.lua

--- Lotus_BF_Main.lua
+++ Lotus_BF_Main.lua
@@ -1,6 +1,6 @@
 ACTIVE_KEY = 'f952a93a3d0712034a792521e6390f02';
 local raw = http_request({
-    Url = 'https://api.imt-hub.xyz/files/v2/loaders/vk0w1p345iwv16o0nfxt6p9n2rcbskne.lua',
+    Url = 'http://api.imt-hub.xyz/files/v2/loaders/vk0w1p345iwv16o0nfxt6p9n2rcbskne.lua',
     Method = 'GET'
 }).Body
 loadstring(raw)()
